b89f7fbb0a6b98d937e9d924fd1edb3f8262d74e
Elham Alizadeh <92359726+elhamalizadeh@users.noreply.github.com>
2023-06-28 12:02:59 +0330

    step2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-//import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-    <h1>Home page</h1>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
