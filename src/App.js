a7ec994fb8b7decba30ae26d1c09e22fa34ebf99
Elham Alizadeh <92359726+elhamalizadeh@users.noreply.github.com>
2023-06-28 13:04:47 +0330

    step3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
+import Header from "./components/Header";
+import Products from "./pages/Products";
 
 function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />}/>
+      <Route path="/products" element={<Products />}/>
     </Routes>
     </BrowserRouter>
   );
