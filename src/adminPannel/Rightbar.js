9f614e53efe4e1dfe9966792fa531cb76c42c596
Elham Alizadeh <92359726+elhamalizadeh@users.noreply.github.com>
2023-07-04 11:38:01 +0330

    rightbar

--- src/adminPannel/Rightbar.js
+++ src/adminPannel/Rightbar.js
@@ -3,7 +3,10 @@ import { Box, Typography } from "@mui/material";
 const Rightbar = () =>{
     return(
         <>
-        <Box flex={2} p={2} sx={{ display:{ xs:"none" , sm:"block"} ,backgroundColor:"pink" , color:"blue"}} >
+        <Box flex={2} p={2} sx={{ display:{ xs:"none" , sm:"block"} ,backgroundColor:"pink"}} >
+        <Box position="fixed">
+          <Typography variant="h6" fontWeight={100}>OnlineFriends</Typography>
+        </Box>
         </Box>
         
         </>
